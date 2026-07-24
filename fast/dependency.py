d37e56f8ab60c8a1aaf749f84342178bea80e270
Arbaz <marbaz00471@gmail.com>
2026-07-24 23:26:23 +0100

    add dependency injection and item management endpoints

---
+++ fast/dependency.py
@@ -0,0 +1,53 @@
+from typing import Annotated, Any
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+
+
+
+# TODO status_code
+@app.post("/items/", status_code=201)
+async def create_item(name: str):
+    return {"name": name}
+
+
+# TODO response_model
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+
+@app.get("/items/", response_model=list[Item])
+async def read_items() -> Any:
+    return [
+        {"name": "Portal Gun", "price": 42.0},
+        {"name": "Plumbus", "price": 32.0},
+    ]
+
+
+# TODO: dependency used for sharing logic or avoiding redundancy
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+avoid_lil_bitredundency = Annotated[dict, Depends(common_parameters)]
+
+@app.get("/items/")
+async def read_items(commons: avoid_lil_bitredundency):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
+
+
